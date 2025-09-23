03ebd4a506e0f30645556ea600dcc8d24912bd9e
VreshG <veereshanand.g@outlook.com>
2025-09-23 23:53:30 +0530

    [PR DEMO 4] Add fourth demo comment to GET and POST handlers for Jenkins notification test

--- api/get_handler.go
+++ api/get_handler.go
@@ -9,6 +9,7 @@ import (
 // [PR DEMO] This comment added to test Jenkins and PR notification
 // [PR DEMO 2] Second demo comment for another commit
 // [PR DEMO 3] Third demo comment for Jenkins notification test
+// [PR DEMO 4] Fourth demo comment for Jenkins notification test
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Dummy log: GET handler called for Jenkins diff test")
 	msg := getSharedMessage()
--- api/post_handler.go
+++ api/post_handler.go
@@ -16,5 +16,6 @@ func getSharedMessage() string {
 	// [PR DEMO] This comment added to test Jenkins and PR notification
 	// [PR DEMO 2] Second demo comment for another commit
 	// [PR DEMO 3] Third demo comment for Jenkins notification test
+	// [PR DEMO 4] Fourth demo comment for Jenkins notification test
 	return "This is a shared message between GET and POST handlers."
 }
