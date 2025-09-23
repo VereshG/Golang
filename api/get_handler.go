12a6c60c456ef191167df657e4080145310efbbe
VreshG <veereshanand.g@outlook.com>
2025-09-24 00:13:23 +0530

    test- 2

--- api/get_handler.go
+++ api/get_handler.go
@@ -5,11 +5,6 @@ import (
 	"net/http"
 )
 
-// GetHandler depends on PostHandler for some shared logic
-// [PR DEMO] This comment added to test Jenkins and PR notification
-// [PR DEMO 2] Second demo comment for another commit
-// [PR DEMO 3] Third demo comment for Jenkins notification test
-// [PR DEMO 4] Fourth demo comment for Jenkins notification test
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Dummy log: GET handler called for Jenkins diff test")
 	msg := getSharedMessage()
--- api/post_handler.go
+++ api/post_handler.go
@@ -13,9 +13,5 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getSharedMessage() string {
-	// [PR DEMO] This comment added to test Jenkins and PR notification
-	// [PR DEMO 2] Second demo comment for another commit
-	// [PR DEMO 3] Third demo comment for Jenkins notification test
-	// [PR DEMO 4] Fourth demo comment for Jenkins notification test
 	return "This is a shared message between GET and POST handlers."
 }
