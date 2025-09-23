aed34bbb4573e358db8111efe6551d2dabb8fd52
VreshG <veereshanand.g@outlook.com>
2025-09-24 00:21:45 +0530

    sfsdfd

--- api/get_handler.go
+++ api/get_handler.go
@@ -6,6 +6,9 @@ import (
 )
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
+	// getSharedMessage is defined in post_handler.go
+	// getSharedMessage is defined in post_handler.go
+	// getSharedMessage is defined in post_handler.go
 	fmt.Println("Dummy log: GET handler called for Jenkins diff test")
 	msg := getSharedMessage()
 	fmt.Fprintf(w, "GET request received. Shared message: %s", msg)
