144879b3ad3f9d5a41a4950b7035a479d178e1c3
VreshG <veereshanand.g@outlook.com>
2025-09-24 12:26:39 +0530

    get changed

--- api/get_handler.go
+++ api/get_handler.go
@@ -11,7 +11,3 @@ func GetHandler(w http.ResponseWriter, r *http.Request) {
 	msg := getSharedMessage()
 	fmt.Fprintf(w, "GET request received. Shared message: %s", msg)
 }
-
-// getSharedMessage is defined in post_handler.go
-// getSharedMessage is defined in post_handler.go
-// getSharedMessage is defined in post_handler.go
